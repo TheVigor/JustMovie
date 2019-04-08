4db099fafd3275cdd61c6f16d385ea711addd25e
TheVigor <igor.proskuryakov14@gmail.com>
2019-04-08 23:20:48 +0300

    added handling bottom nav clicks and saving instance state nav position

--- app/src/main/java/com/noble/activity/justmovie/MainActivity.kt
+++ app/src/main/java/com/noble/activity/justmovie/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.noble.activity.justmovie
 
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import com.noble.activity.justmovie.extensions.showToast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener  {
+
+    companion object {
+        private const val DEFAULT_FRAGMENT = R.id.item_playing
+        private const val ARG_BOTTOM_BAR_POSITION = "pos"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,8 +22,31 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setTitle(R.string.app_name)
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
 
-
-
+        if (savedInstanceState == null) {
+            bottomNavigationView.selectedItemId = DEFAULT_FRAGMENT
+        } else {
+            bottomNavigationView.selectedItemId = savedInstanceState.getInt(ARG_BOTTOM_BAR_POSITION)
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        outState?.putInt(ARG_BOTTOM_BAR_POSITION, bottomNavigationView.selectedItemId)
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_playing -> showToast("Playing")
+            R.id.item_rated -> showToast("Rated")
+            R.id.item_upcoming -> showToast("Upcoming")
+        }
+
+        return true
+    }
+
+
+
 }
+
