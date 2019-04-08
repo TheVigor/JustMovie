1858393ea39368206e291775e8a310bcf7f55fdb
TheVigor <igor.proskuryakov14@gmail.com>
2019-04-08 22:32:01 +0300

    created empty project

---
+++ app/src/main/java/com/noble/activity/justmovie/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.noble.activity.justmovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
