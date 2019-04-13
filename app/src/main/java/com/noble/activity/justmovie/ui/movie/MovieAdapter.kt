1c1dbe42c67dd4a765373cb52d165d22b8f72fe7
TheVigor <igor.proskuryakov14@gmail.com>
2019-04-13 14:17:41 +0300

    started implementing movies fragment for showing movies list

---
+++ app/src/main/java/com/noble/activity/justmovie/ui/movie/MovieAdapter.kt
@@ -0,0 +1,66 @@
+package com.noble.activity.justmovie.ui.movie
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.noble.activity.justmovie.R
+import com.noble.activity.justmovie.data.TmdbConfig.TMDB_IMAGE
+import com.noble.activity.justmovie.data.model.Movie
+import com.noble.activity.justmovie.ui.common.DebouncingOnClickListener
+import java.util.*
+
+import kotlinx.android.synthetic.main.item_poster.*
+import kotlinx.android.extensions.LayoutContainer
+
+class MovieAdapter(
+    private val listener: Listener
+): RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    interface Listener {
+        fun onMovieClick(movie: Movie)
+    }
+
+    val movies = ArrayList<Movie>()
+
+    fun addMovies(results: List<Movie>) {
+        movies.addAll(results)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_poster, parent, false)
+        val holder = MovieViewHolder(view)
+
+        //val landscape = DeviceUtil.isLandscape(parent.context) || DeviceUtil.isTablet(parent.context)
+        //view.layoutParams.height = if (landscape) (parent.width / 2.5F).toInt() else (parent.width / 2 * 1.5F).toInt()
+
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    override fun getItemCount() = movies.size
+
+    inner class MovieViewHolder(override val containerView: View):
+        RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+        fun bind(movie: Movie) {
+            Glide.with(containerView.context)
+                .load(String.format(Locale.US, TMDB_IMAGE, "w342", movie.posterPath))
+                .thumbnail(0.1F)
+                .into(poster)
+
+            containerView.setOnClickListener(object: DebouncingOnClickListener() {
+                override fun doClick(v: View) {
+                    if (adapterPosition != RecyclerView.NO_POSITION) {
+                        listener.onMovieClick(movies[adapterPosition])
+                    }
+                }
+            })
+        }
+    }
+}
---
+++ app/src/main/java/com/noble/activity/justmovie/ui/movie/MovieFragment.kt
@@ -0,0 +1,88 @@
+package com.noble.activity.justmovie.ui.movie
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.noble.activity.justmovie.R
+import com.noble.activity.justmovie.data.model.Movie
+import com.noble.activity.justmovie.ui.common.GridSpacingItemDecoration
+import kotlinx.android.synthetic.main.fragment_movie.*
+
+class MovieFragment: Fragment(), MovieAdapter.Listener {
+    companion object {
+        const val MOVIE_TYPE = "movie_type"
+
+        fun newInstance(type: String): MovieFragment {
+            val args = Bundle()
+            args.putString(MOVIE_TYPE, type)
+
+            val fragment = MovieFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    private lateinit var type: String
+    private lateinit var adapter: MovieAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = MovieAdapter(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val spanCount = 2
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), spanCount)
+        recyclerView.addItemDecoration(GridSpacingItemDecoration(spanCount, resources.getDimension(R.dimen.movies_list_spacing).toInt()))
+        recyclerView.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1) && adapter.itemCount != 0) {
+                    //presenter.moviesNext(0, list)
+                }
+            }
+        })
+
+        //emptyView.setOnClickListener { presenter.movies(0, list) }
+
+        //type = arguments?.getString(MOVIE_TYPE) ?: NOW_PLAYING
+        //presenter.movies(0, list)
+    }
+
+    override fun onMovieClick(movie: Movie) {
+        val intent = Intent(requireContext(), MovieActivity::class.java)
+        intent.putExtra(MOVIE_TYPE, movie)
+        startActivity(intent)
+    }
+
+//    override fun loading(state: Boolean) {
+//        progressBar.visibility = if (state) VISIBLE else GONE
+//    }
+//
+//    override fun content(results: List<Movie>) {
+//        adapter.addMovies(results)
+//    }
+//
+//    override fun error(code: Int) {
+//        emptyView.visibility = VISIBLE
+//        emptyView.setMode(code)
+//
+//        if (BuildUtil.isApiKeyEmpty()) {
+//            emptyView.setValue(R.string.error_empty_api_key)
+//        }
+//    }
+//
+
+}
