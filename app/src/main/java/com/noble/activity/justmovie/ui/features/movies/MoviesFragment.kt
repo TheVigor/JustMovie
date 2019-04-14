b42207db51b4f411abb815b580cb272907dbd1a2
TheVigor <igor.proskuryakov14@gmail.com>
2019-04-14 12:19:28 +0300

    added view model for movies fragment

--- app/src/main/java/com/noble/activity/justmovie/ui/features/movies/MoviesFragment.kt
+++ app/src/main/java/com/noble/activity/justmovie/ui/features/movies/MoviesFragment.kt
@@ -4,14 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.noble.activity.justmovie.R
 import com.noble.activity.justmovie.data.model.Movie
+import com.noble.activity.justmovie.data.model.MoviesResponse.Companion.NOW_PLAYING
+import com.noble.activity.justmovie.data.remote.TmdbClient
 import com.noble.activity.justmovie.ui.common.GridSpacingItemDecoration
 import kotlinx.android.synthetic.main.fragment_movie.*
+
 
 class MoviesFragment: Fragment(), MoviesAdapter.Listener {
     companion object {
@@ -30,10 +38,8 @@ class MoviesFragment: Fragment(), MoviesAdapter.Listener {
     private lateinit var type: String
     private lateinit var adapter: MoviesAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        adapter = MoviesAdapter(this)
-    }
+    private lateinit var moviesViewModel: MoviesViewModel
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_movie, container, false)
@@ -41,7 +47,9 @@ class MoviesFragment: Fragment(), MoviesAdapter.Listener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val spanCount = 2
+        val spanCount = 3
+
+        adapter = MoviesAdapter(this)
 
         recyclerView.adapter = adapter
         recyclerView.layoutManager = GridLayoutManager(requireContext(), spanCount)
@@ -55,10 +63,40 @@ class MoviesFragment: Fragment(), MoviesAdapter.Listener {
             }
         })
 
-        //emptyView.setOnClickListener { presenter.movies(0, list) }
+        emptyView.setOnClickListener { moviesViewModel.movies(0, type) }
 
-        //type = arguments?.getString(MOVIE_TYPE) ?: NOW_PLAYING
-        //presenter.movies(0, list)
+        moviesViewModel = ViewModelProviders.of(this, MoviesViewModelFactory(
+            MoviesRepository.getInstance(TmdbClient.getInstance())))
+            .get(MoviesViewModel::class.java)
+
+        moviesViewModel.loadingState.observe(viewLifecycleOwner,
+            Observer { state ->
+                state?.let {
+                    progressBar.visibility = if (state) VISIBLE else GONE
+                }
+            })
+
+        moviesViewModel.errorState.observe(viewLifecycleOwner,
+            Observer { code ->
+                code?.let {
+                    emptyView.visibility = VISIBLE
+                    emptyView.setMode(code)
+
+//                    if (BuildUtil.isApiKeyEmpty()) {
+//                        emptyView.setValue(R.string.error_empty_api_key)
+//                    }
+                }
+            })
+
+        moviesViewModel.contentState.observe(viewLifecycleOwner,
+            Observer { content ->
+                content?.let {
+                    adapter.addMovies(content)
+                }
+            })
+
+        type = arguments?.getString(MOVIE_TYPE) ?: NOW_PLAYING
+        moviesViewModel.movies(0, type)
     }
 
     override fun onMovieClick(movie: Movie) {
@@ -66,23 +104,5 @@ class MoviesFragment: Fragment(), MoviesAdapter.Listener {
         intent.putExtra(MOVIE_TYPE, movie)
         startActivity(intent)
     }
-
-//    override fun loading(state: Boolean) {
-//        progressBar.visibility = if (state) VISIBLE else GONE
-//    }
-//
-//    override fun content(results: List<Movie>) {
-//        adapter.addMovies(results)
-//    }
-//
-//    override fun error(code: Int) {
-//        emptyView.visibility = VISIBLE
-//        emptyView.setMode(code)
-//
-//        if (BuildUtil.isApiKeyEmpty()) {
-//            emptyView.setValue(R.string.error_empty_api_key)
-//        }
-//    }
-//
 
 }
---
+++ app/src/main/java/com/noble/activity/justmovie/ui/features/movies/MoviesViewModel.kt
@@ -0,0 +1,138 @@
+package com.noble.activity.justmovie.ui.features.movies
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.noble.activity.justmovie.data.model.Movie
+import com.noble.activity.justmovie.ui.base.BaseViewModel
+import com.noble.activity.justmovie.ui.errors.EmptyViewMode
+import com.noble.activity.justmovie.ui.utils.NetworkUtils
+import retrofit2.HttpException
+
+class MoviesViewModel(private val repository: MoviesRepository): BaseViewModel(),
+    MoviesContract.ViewModelContract {
+
+    private var page: Int = 0
+
+    private var _loadingState = MutableLiveData<Boolean>()
+    val loadingState: LiveData<Boolean>
+            get() = _loadingState
+
+    private var _errorState = MutableLiveData<Int>()
+    val errorState: LiveData<Int>
+        get() = _errorState
+
+    private var _contentState = MutableLiveData<List<Movie>>()
+    val contentState: LiveData<List<Movie>>
+        get() = _contentState
+
+    override fun movies(movieId: Int, list: String) {
+        if (!NetworkUtils.isNetworkConnected()) {
+            _errorState.value = EmptyViewMode.MODE_NO_CONNECTION
+            return
+        }
+
+        page = 1
+        addDisposable(repository.movies(movieId, list, page)
+            .doOnSubscribe { _loadingState.value = true }
+            .doAfterTerminate { _loadingState.value = false }
+            .subscribe({
+                if (it.isEmpty()) {
+                    _errorState.value = EmptyViewMode.MODE_NO_MOVIES
+                    return@subscribe
+                }
+                _contentState.value = it
+            }, { _errorState.value = EmptyViewMode.MODE_NO_MOVIES }))
+    }
+
+    override fun moviesNext(movieId: Int, list: String) {
+        if (NetworkUtils.isNetworkConnected().not()) return
+
+        page++
+        addDisposable(repository.movies(movieId, list, page).subscribe { _contentState.value =it })
+    }
+
+    override fun movies(keywordId: Int) {
+        if (NetworkUtils.isNetworkConnected().not()) {
+            _errorState.value = EmptyViewMode.MODE_NO_CONNECTION
+            return
+        }
+
+        page = 1
+        addDisposable(repository.movies(keywordId, page)
+            .doOnSubscribe { _loadingState.value = true }
+            .doAfterTerminate { _loadingState.value = false }
+            .switchIfEmpty { _errorState.value = EmptyViewMode.MODE_NO_MOVIES }
+            .subscribe({ _contentState.value = it }, { _errorState.value = EmptyViewMode.MODE_NO_MOVIES }))
+    }
+
+    override fun moviesNext(keywordId: Int) {
+        if (NetworkUtils.isNetworkConnected().not()) {
+            return
+        }
+
+        page++
+        addDisposable(repository.movies(keywordId, page).subscribe { _contentState.value = it })
+    }
+
+    override fun moviesWatchlist(accountId: Int, sessionId: String) {
+        /*if (!NetworkUtil.isNetworkConnected()) {
+            view?.error(EmptyViewMode.MODE_NO_CONNECTION)
+            return
+        }*/
+
+        page = 1
+        addDisposable(repository.moviesWatchlist(accountId, sessionId, page)
+            .doOnSubscribe { _loadingState.value = true }
+            .doAfterTerminate { _loadingState.value = false }
+            .subscribe({
+                if (it.isEmpty()) {
+                    _errorState.value = 1
+                    return@subscribe
+                }
+                _contentState.value = it }, { _errorState.value = (it as HttpException).code() }
+            )
+        )
+    }
+
+    override fun moviesWatchlistNext(accountId: Int, sessionId: String) {
+        if (!NetworkUtils.isNetworkConnected()) return
+
+        page++
+        addDisposable(repository.moviesWatchlist(accountId, sessionId, page)
+            .subscribe { _contentState.value = it })
+    }
+
+    override fun moviesFavorite(accountId: Int, sessionId: String) {
+        /*if (NetworkUtil.isNetworkConnected().not()) {
+            view?.error(Throwable("no connection"))
+            return
+        }*/
+
+        page = 1
+        addDisposable(repository.moviesFavorite(accountId, sessionId, page)
+            .doOnSubscribe { _loadingState.value = true }
+            .doAfterTerminate { _loadingState.value = false }
+            .subscribe(
+                {
+                    if (it.isEmpty()) {
+                        _errorState.value = 1
+                        return@subscribe
+                    }
+                    _contentState.value = it },
+                { _errorState.value = (it as HttpException).code()}
+            )
+        )
+    }
+
+    override fun moviesFavoriteNext(accountId: Int, sessionId: String) {
+        if (NetworkUtils.isNetworkConnected().not()) {
+            return
+        }
+
+        page++
+        addDisposable(repository.moviesFavorite(accountId, sessionId, page)
+            .subscribe { _contentState.value = it })
+    }
+
+
+}
