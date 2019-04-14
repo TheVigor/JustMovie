b10cea92a3d356a6412554000eb7550c14858c0c
TheVigor <igor.proskuryakov14@gmail.com>
2019-04-14 12:15:05 +0300

    added disposable view model

---
+++ app/src/main/java/com/noble/activity/justmovie/ui/base/BaseViewModel.kt
@@ -0,0 +1,16 @@
+package com.noble.activity.justmovie.ui.base
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+abstract class BaseViewModel : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+
+    fun addDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
+    override fun onCleared() {
+        compositeDisposable.clear()
+    }
+}
